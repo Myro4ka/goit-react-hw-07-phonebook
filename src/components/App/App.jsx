5dd38c2187d913a8625317afe318ec0cdc140720
Myro4ka <www.mirosya@gmail.com>
2023-01-27 19:37:05 +0100

    add initial functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import { Component } from 'react';
 import { Section } from 'components/Section/Section';
-import { Phonebook } from 'components/Phonebook/Phonebook';
-import { Contacts } from 'components/Contacts/Contacts';
+// import { Phonebook } from 'components/Phonebook/Phonebook';
+// import { Contacts } from 'components/Contacts/Contacts';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
@@ -9,14 +10,39 @@ export class App extends Component {
     name: '',
   };
 
+  handleAddToContacts = () => {
+    const newContact = { name: this.state.name, id: nanoid() };
+    this.setState(prev => ({
+      contacts: [...prev.contacts, newContact],
+      name: '',
+    }));
+  };
+
   render() {
     return (
       <>
         <Section title="Phonebook">
-          <Phonebook />
+          <div>
+            <p>Name</p>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              onChange={e => this.setState({ name: e.target.value })}
+              value={this.state.name}
+              required
+            />
+            <button onClick={this.handleAddToContacts}>Add contact</button>
+          </div>
         </Section>
         <Section title="Contacts">
-          <Contacts />
+          <ul>
+            {this.state.contacts.map(contact => {
+              console.log(contact.id);
+              return <li key={contact.id}>{contact.name}</li>;
+            })}
+          </ul>
         </Section>
       </>
     );
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -8,8 +8,9 @@ export const Phonebook = () => {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
+        onChange={e => console.log(e.target.value)}
       />
-      <button>Add contact</button>
+      <button onClick={console.log}>Add contact</button>
     </div>
   );
 };
