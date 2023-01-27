479abbd91d3641d1f5280cbb32438a5437951932
Myro4ka <www.mirosya@gmail.com>
2023-01-27 20:43:57 +0100

    add input for a phone number

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,11 +11,20 @@ export class App extends Component {
   };
 
   handleAddToContacts = () => {
-    const newContact = { name: this.state.name, id: nanoid() };
+    const newContact = {
+      name: this.state.name,
+      number: this.state.number,
+      id: nanoid(),
+    };
     this.setState(prev => ({
       contacts: [...prev.contacts, newContact],
       name: '',
+      number: '',
     }));
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
@@ -29,8 +38,18 @@ export class App extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              onChange={e => this.setState({ name: e.target.value })}
+              onChange={this.handleChange}
               value={this.state.name}
+              required
+            />
+            <p>Number</p>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              onChange={this.handleChange}
+              value={this.state.number}
               required
             />
             <button onClick={this.handleAddToContacts}>Add contact</button>
@@ -40,7 +59,11 @@ export class App extends Component {
           <ul>
             {this.state.contacts.map(contact => {
               console.log(contact.id);
-              return <li key={contact.id}>{contact.name}</li>;
+              return (
+                <li key={contact.id}>
+                  {contact.name}: {contact.number}
+                </li>
+              );
             })}
           </ul>
         </Section>
